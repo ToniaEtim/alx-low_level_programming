459cabb4205a74e2df3a1cef2b8fb575ea340a83
ToniaEtim <abasimatim4@gmail.com>
2022-09-08 21:28:04 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{ 
+char str1[54];
+strcpy(str1, "Programming is like building a multilingual puzzle/n ");
+puts(str1);
+return(0);
+}
+
+    	    
+	
