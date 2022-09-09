d60edb66c20e2da92ac2527010a2ffaae4902a04
ToniaEtim <abasimatim4@gmail.com>
2022-09-09 07:38:38 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-
 /**
- * main print strings in the function
- *
- * Description: using the function
- * print the bytes of all
- * Return:0
- */
-int main(void) 
+* main- print string in the function
+* 
+*description :using the function
+*print the bytes of all
+*return: 0
+*/
+int main(void)
 {
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
 
-  char c;
-  int i;
-  long li;
-  long long lli;
-  float f;
-
-  printf("size of a char: %ld byte(s)\n", sizeof(c));
-  printf("size of an int: %ld byte(s)\n", sizeof(i));
-  printf("size of a long int: %ld byte(s)\n", sizeof(li));
-  printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
-  printf("size of a float: %ld byte(s)\n", sizeof(f));
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
 }
