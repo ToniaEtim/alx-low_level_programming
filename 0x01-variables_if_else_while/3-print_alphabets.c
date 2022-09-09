85e8d649145b699ebeb26a02f58d54779962ab39
ToniaEtim <abasimatim4@gmail.com>
2022-09-09 16:47:09 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - print the alphabets
+*
+* Description: using the main function
+* this programme will print the lower and upper case alphabets
+* Return: 0
+*/
+int main(void)
+{
+char ch;
