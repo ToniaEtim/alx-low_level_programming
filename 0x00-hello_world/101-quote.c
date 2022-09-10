a023360370ae8127641b92c9cb2fb67648100cde
ToniaEtim <abasimatim4@gmail.com>
2022-09-10 06:13:46 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - print a quote using the main function
+* main -print the qupte in the function
 *
-* Description: using the main function
+*Description: using the main function
 * print the quote
 * Return: 1
 */
-int main(void)	
+int main(void)
 {
-	write(2, "and that piece was useful - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that peice of art is useful - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
