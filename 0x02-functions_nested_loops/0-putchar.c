943fc3046dcb5410662d7a8f9b72a91212a6066a
ToniaEtim <abasimatim4@gmail.com>
2022-09-13 15:03:55 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,15 @@
 */
 int main(void)
 {
-char c[] = "_putchar";
-int i = 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-for (i = 0; i <= 7; i++)
-{
-putchar(c[i]);
-}
-putchar('\n');
-return (0);
+	return(0);
 }
