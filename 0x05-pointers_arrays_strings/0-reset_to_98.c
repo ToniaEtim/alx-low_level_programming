5f6206e8831b74b2affa64dbacc96691bdf0774e
ToniaEtim <abasimatim4@gmail.com>
2022-09-19 15:23:15 -0700

    0-reset_to_98.cv

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
-* main - check the code 
-*
+* reset_to_98 - function that takes a pointer to an int
+*	updates the value it pointd to 98
+* @n: The pointer to an int
 * Return: Always 0
 */
 void reset_to_98(int *n)
 {
- *n = 98;
-
-return (0)
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@
 *char  prototypes(char)
 */
 
-int _putchar(char)
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
