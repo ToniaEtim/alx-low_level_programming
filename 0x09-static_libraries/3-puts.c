b584014a0a1816b643c7000aeb600aa22a2d65ea
ToniaEtim <abasimatim4@gmail.com>
2022-10-03 08:03:25 -0700

    static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,13 +2,13 @@
 
 /**
  * _puts - writes function that prints a string to stdout
- * @str: The string to be printed
+ * @s: The string to be printed
  */
 
 void _puts(char *s)
 {
 	while (*s)
-		_putchar(*s+);
+		_putchar(*s);
 
 	_putchar('\n');
 }
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -5,12 +5,12 @@
 * @c: is the int that will use for the argument of the function
 * Return: 0
 */
-int _abs(int c)
+int _abs(int n)
 {
-	if (c > 0 || c == 0)
+	if (n > 0 || n == 0)
 	{
-	return (c);
+	return (n);
 	}
 	else
-	return (c * -1);
+	return (n * -1);
 }
